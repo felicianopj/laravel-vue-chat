c914e628a8c52e328e5a1b9bd5da5324bc3fdfff
Feliciano Prochera <feliciano.sjr@gmail.com>
2016-09-13 11:09:59 -0300

    Clean up

---
+++ app/Events/MessageSent.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Events;
+
+use App\Message;
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+
+class MessageSent implements ShouldBroadcast
+{
+    use InteractsWithSockets, SerializesModels;
+
+    public $message;
+
+    /**
+     * Create a new event instance.
+     *
+     * @return void
+     */
+    public function __construct(Message $message)
+    {
+        $this->message = $message;
+
+        $this->dontBroadcastToCurrentUser();
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return Channel|array
+     */
+    public function broadcastOn()
+    {
+        // return new PrivateChannel('channel-name');
+        return ['chat.1.messages'];
+    }
+}
